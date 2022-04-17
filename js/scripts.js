c782bfe959031e6ad6ff689847e88c05458005a5
AbdoulBOUCHOUROINE <102979190+AbdoulBOUCHOUROINE@users.noreply.github.com>
2022-04-17 10:19:06 +0200

    Add files via upload
    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,145 @@
+/*!
+* Start Bootstrap - Stylish Portfolio v6.0.5 (https://startbootstrap.com/theme/stylish-portfolio)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-stylish-portfolio/blob/master/LICENSE)
+*/
+window.addEventListener('DOMContentLoaded', event => {
+
+    const sidebarWrapper = document.getElementById('sidebar-wrapper');
+    let scrollToTopVisible = false;
+    // Closes the sidebar menu
+    const menuToggle = document.body.querySelector('.menu-toggle');
+    menuToggle.addEventListener('click', event => {
+        event.preventDefault();
+        sidebarWrapper.classList.toggle('active');
+        _toggleMenuIcon();
+        menuToggle.classList.toggle('active');
+    })
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    var scrollTriggerList = [].slice.call(document.querySelectorAll('#sidebar-wrapper .js-scroll-trigger'));
+    scrollTriggerList.map(scrollTrigger => {
+        scrollTrigger.addEventListener('click', () => {
+            sidebarWrapper.classList.remove('active');
+            menuToggle.classList.remove('active');
+            _toggleMenuIcon();
+        })
+    });
+
+    function _toggleMenuIcon() {
+        const menuToggleBars = document.body.querySelector('.menu-toggle > .fa-bars');
+        const menuToggleTimes = document.body.querySelector('.menu-toggle > .fa-xmark');
+        if (menuToggleBars) {
+            menuToggleBars.classList.remove('fa-bars');
+            menuToggleBars.classList.add('fa-xmark');
+        }
+        if (menuToggleTimes) {
+            menuToggleTimes.classList.remove('fa-xmark');
+            menuToggleTimes.classList.add('fa-bars');
+        }
+    }
+
+    // Scroll to top button appear
+    document.addEventListener('scroll', () => {
+        const scrollToTop = document.body.querySelector('.scroll-to-top');
+        if (document.documentElement.scrollTop > 100) {
+            if (!scrollToTopVisible) {
+                fadeIn(scrollToTop);
+                scrollToTopVisible = true;
+            }
+        } else {
+            if (scrollToTopVisible) {
+                fadeOut(scrollToTop);
+                scrollToTopVisible = false;
+            }
+        }
+    })
+})
+
+function fadeOut(el) {
+    el.style.opacity = 1;
+    (function fade() {
+        if ((el.style.opacity -= .1) < 0) {
+            el.style.display = "none";
+        } else {
+            requestAnimationFrame(fade);
+        }
+    })();
+};
+
+function fadeIn(el, display) {
+    el.style.opacity = 0;
+    el.style.display = display || "block";
+    (function fade() {
+        var val = parseFloat(el.style.opacity);
+        if (!((val += .1) > 1)) {
+            el.style.opacity = val;
+            requestAnimationFrame(fade);
+        }
+    })();
+};
+
+class myHeader extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = `
+        <header class="masthead d-flex align-items-center">
+            <div class="container px-4 px-lg-5 text-center">
+                <h1 class="mb-1">Mon Portfolio</h1>
+                <h3 class="mb-5"><em>Bienvenu dans mon univers!</em></h3>
+                <a class="btn btn-primary btn-xl" href="#about">EN SAVOIR PLUS</a>
+                
+            </div>
+        </header>
+`
+    }
+};
+
+customElements.define('my-header', myHeader)
+
+
+class myFooter extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = `
+        <footer class="footer text-center">
+        <div class="container px-4 px-lg-5">
+            <ul class="list-inline mb-5">
+                <li class="list-inline-item">
+                    <a class="social-link rounded-circle text-white mr-3" href="#!"><i class="icon-social-facebook"></i></a>
+                </li>
+                <li class="list-inline-item">
+                    <a class="social-link rounded-circle text-white mr-3" href="#!"><i class="icon-social-twitter"></i></a>
+                </li>
+                <li class="list-inline-item">
+                    <a class="social-link rounded-circle text-white" href="#!"><i class="icon-social-github"></i></a>
+                </li>
+            </ul>
+            <p class="text-muted small mb-0">Copyright &copy; Abdoul BOUCHOUROINE Website 2022</p>
+        </div>
+    </footer> 
+`
+    }
+};
+
+customElements.define('my-footer', myFooter)
+
+
+class myNavRight extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = `
+        <a class="menu-toggle rounded" href="#"><i class="fas fa-bars"></i></a>
+        <nav id="sidebar-wrapper">
+            <ul class="sidebar-nav">
+                <li class="sidebar-brand"><a href="index.html">MON PORTFOLIO</a></li>
+                <li class="sidebar-nav-item"><a href="index.html">ACCUEIL</a></li>
+                <li class="sidebar-nav-item"><a href="#about">À PROPOS</a></li>
+                <li class="sidebar-nav-item"><a href="index.html#services">SERVICES</a></li>
+                <li class="sidebar-nav-item"><a href="index.html#portfolio">PORTFOLIO</a></li>
+                <li class="sidebar-nav-item"><a href="contact.html">CONTACT</a></li>
+            </ul>
+        </nav>
+`
+    }
+};
+
+customElements.define('my-navright', myNavRight)
+
